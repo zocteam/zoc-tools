9cb5f8d3f628112f9ead442ccd14e1f8078a2e0c
deadthings <33961641+deadthings@users.noreply.github.com>
2018-10-03 18:30:24 -0400

    Multiple updates to setup_zoc_mn.sh
    - Reorganized script to reflect execution logic
    - Added sudo install
    - Added dnsutils package for dig command
    - Further improvements to printed text
    - Added clarity to pre-existing privkey instructions
    - Revised method for adding mnchecker to cron
    - Reduced maxconnections from 25 to 16 to help with resource allocation on small VPS servers
    - Changed sequence to install sentinel earlier in the process
    - Relevant info printed at end of script all in one place

--- setup_zoc_mn.sh
+++ setup_zoc_mn.sh
@@ -4,75 +4,18 @@ echo "                     01coin Masternode Setup Shell Script"
 echo "                              Created by Evydder"
 echo "*******************************************************************************"
 
-configQuestions()
-{
-	echo "*******************************************************************************"
-	echo "                                    Config"
-	echo "*******************************************************************************"
-
-	#IP
-	vpsip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-	while true; do
-		read -p "Is this your VPS IP address? ${vpsip} [Y/N]" yn
-		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) echo "Please type in your VPS IP address"; read vpsip;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-
-	rpcuser=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
-	rpcpassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
-	
-	#Needed to be appended to the file before ask for incase of want to gen key Privatekey
-	if [ ! -d ".zeroonecore" ]; then
-		mkdir .zeroonecore 
-	fi
-	
-	echo "rpcuser=${rpcuser}" >> .zeroonecore/zeroone.conf
-	echo "rpcpassword=${rpcpassword}" >> .zeroonecore/zeroone.conf
-	
-	while true; do
-		read -p "Would you like to provide a private key (if you already have one)? ${privkey} [Y/N]" yn
-		case $yn in
-			[Yy]* ) askforprivatekey;break;;
-			[Nn]* ) genkey;break;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-
-	echo "**************************************************************"
-	while true; do
-		read -p "Would you like a shell scipt to start the node?  [Y/N]" yn
-		case $yn in
-			[Yy]* ) echo "zeroone/zerooned -daemon ">> startZeroOne.sh;askstartonboot;break;;
-			[Nn]* ) break;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-	
-	echo "**************************************************************"
-	while true; do
-		read -p "Would you like to install a node manager to keep the blockchain synced?  [Y/N]" yn
-		case $yn in
-			[Yy]* ) setup_manager;break;;
-			[Nn]* ) break;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-}
-
 install_preqs()
 {
 	echo "*******************************************************************************"
 	echo "                           Installing Requirements"
 	echo "*******************************************************************************"
 
+	apt install -y sudo
 	sudo apt install -y software-properties-common
 	sudo add-apt-repository -y ppa:bitcoin/bitcoin
 	sudo apt update 
 	sudo apt upgrade -y 
-	sudo apt install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libdb4.8-dev libdb4.8++-dev python-virtualenv nano git openssl
+	sudo apt install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libdb4.8-dev libdb4.8++-dev python-virtualenv nano git openssl dnsutils
 }
 
 download()
@@ -86,13 +29,157 @@ download()
 	#Only needed for 16.04
 		sudo apt-get install -y libminiupnpc-dev 
 	fi
-	mkdir zeroone
+	mkdir ~/zeroone
 	wget http://files.01coin.io/build/linux/zeroonecore-0.12.3-x86_64-linux-gnu.tar.gz
 	tar -xvf zeroonecore-0.12.3-x86_64-linux-gnu.tar.gz
-	rm zeroonecore-0.12.3-x86_64-linux-gnu.tar.gz
-	mv zeroonecore-0.12.3/bin/* zeroone/
+	rm ~/zeroonecore-0.12.3-x86_64-linux-gnu.tar.gz
+	mv ~/zeroonecore-0.12.3/bin/* ~/zeroone/
 	
-	mkdir .zeroonecore
+	mkdir ~/.zeroonecore
+}
+
+compile()
+{
+#checks ram and makes swap if nessary
+	ram="$(awk '/MemTotal/ {print $2}' /proc/meminfo)"
+	minram="1048576"
+	if [ "$ram" -le "$minram" ]
+	then
+		dd if=/dev/zero of=/swapfile count=2048 bs=1M
+		chmod 600 /swapfile
+		mkswap /swapfile
+		swapon /swapfile
+		echo "/swapfile   none    swap    sw    0   0" > /etc/fstab
+	fi
+	# Download and Compile 
+	git clone https://github.com/zocteam/zeroonecoin
+	cd ~/zeroonecoin
+	sudo ./autogen.sh
+	sudo ./configure CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768"
+	cpucores = grep -c ^processor /proc/cpuinfo
+	sudo make -j$cpucores
+	mkdir ~/zeroone
+	mv ~/zeroonecoin/src/zerooned ~/zeroone/zerooned
+	mv ~/zeroonecoin/src/zeroone-cli ~/zeroone/zeroone-cli
+	mv ~/zeroonecoin/src/zeroone-tx ~/zeroone/zeroone-tx
+}
+
+configQuestions()
+{
+	echo "*******************************************************************************"
+	echo "                                    Config"
+	echo "*******************************************************************************"
+
+	#IP
+	vpsip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+	while true; do
+		read -p "Is this your VPS IP address? ${vpsip} [Y/N] " yn
+		case $yn in
+			[Yy]* ) break;;
+			[Nn]* ) echo "Please type in your VPS IP address below:"; read vpsip;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	rpcuser=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+	rpcpassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+	
+	#Needed to be appended to the file before ask for incase of want to gen key Privatekey
+	if [ ! -d ".zeroonecore" ]; then
+		mkdir ~/.zeroonecore 
+	fi
+	
+	echo "rpcuser=${rpcuser}" >> .zeroonecore/zeroone.conf
+	echo "rpcpassword=${rpcpassword}" >> .zeroonecore/zeroone.conf
+	
+	echo "**************************************************************"
+	while true; do
+		read -p "Would you like to provide a private key (if you already have one)? ${privkey} [Y/N] " yn
+		case $yn in
+			[Yy]* ) echo "Please type in your private key below:"; askforprivatekey;break;;
+			[Nn]* ) genkey;break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	echo "**************************************************************"
+	while true; do
+		read -p "Would you like a shell scipt to start the node?  [Y/N] " yn
+		case $yn in
+			[Yy]* ) echo "zeroone/zerooned -daemon ">> startZeroOne.sh;askstartonboot;break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+	
+	echo "**************************************************************"
+	while true; do
+		read -p "Would you like to install a node manager to keep the blockchain synced?  [Y/N] " yn
+		case $yn in
+			[Yy]* ) setup_manager;break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+}
+
+askforprivatekey()
+{
+#Masternode Priv Key
+	read privkey
+	while true; do
+		read -p "Is this the correct private key? ${privkey} [Y/N] " yn
+		case $yn in
+			[Yy]* ) break;;
+			[Nn]* ) echo "Please type in your VPS IP address below:"; read privkey;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+}
+
+askstartonboot()
+{
+	echo "**************************************************************"
+	while true; do
+		read -p "Would you like to start the node automatically on boot?  [Y/N] " yn
+		case $yn in
+			[Yy]* ) startonboot;break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+}
+
+startonboot()
+{
+	chmod -x startZeroOne.sh
+	chmod 777 startZeroOne.sh
+	sudo echo "@reboot /root/startZeroOne.sh" >> /etc/crontab
+}
+
+genkey()
+{
+		zeroone/zerooned -daemon
+		echo "Please wait while the 01coin daemon generates a new private key..."
+		sleep 10
+		while ! zeroone/zeroone-cli getinfo; do
+			sleep 10
+		done
+		privkey=$(zeroone/zeroone-cli masternode genkey)
+		killall -9 zerooned
+}
+
+setup_manager()
+{
+	killall zerooned
+	wget https://raw.githubusercontent.com/zocteam/zoc-tools/master/mnchecker
+	chmod 777 mnchecker
+	echo "rpcport=10101" >> .zeroonecore/zeroone.conf
+
+	crontab -l > mncheckercron
+	echo "*/10 * * * * /root/mnchecker --currency-bin-cli=/root/zeroone/zeroone-cli --currency-bin-daemon=/root/zeroone/zerooned --currency-datadir=.zeroonecore" >> mncheckercron
+	crontab mncheckercron
+	rm mncheckercron
 }
 
 config()
@@ -104,7 +191,7 @@ config()
 	echo "externalip=${vpsip}:10000" >> .zeroonecore/zeroone.conf
 	echo "masternode=1" >> .zeroonecore/zeroone.conf
 	echo "masternodeprivkey=${privkey}" >> .zeroonecore/zeroone.conf
-	echo "maxconnections=25" >> .zeroonecore/zeroone.conf
+	echo "maxconnections=16" >> .zeroonecore/zeroone.conf
 }
 
 sentinel()
@@ -115,18 +202,18 @@ sentinel()
 	
 	cd ~
 	
-	sudo apt-get update
-	sudo apt-get -y install python-virtualenv virtualenv
+	sudo apt install -y python-virtualenv virtualenv
 	
 	git clone https://github.com/zocteam/sentinel.git zoc_sentinel
 	cd zoc_sentinel
 	virtualenv ./venv
 	./venv/bin/pip install -r requirements.txt
-
+	
 	crontab -l > mycron
 	echo "* * * * * cd $(pwd) && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py >> zoc_sentinel.log >/dev/null 2>&1" >> mycron
 	crontab mycron
 	rm mycron
+	cd ~
 }
 
 start_mn()
@@ -136,11 +223,8 @@ start_mn()
 	echo "*******************************************************************************"
 
 	zeroone/zerooned -daemon -assumevalid=0000000005812118515c654ab36f46ef2b7b3732a6115271505724ff972417c7
-
 	echo 'If the above says "ZeroOne Core server starting" then masternode is installed' 
-	echo "In your wallet please append the following to masternode.conf"
-	echo ""
-	echo "MN-X ${vpsip}:10000 ${privkey} collateral_output_txid collateral_output_index"
+
 }
 
 adds_nodes()
@@ -161,78 +245,16 @@ adds_nodes()
 	exit 1
 }
 
-compile()
-{
-#checks ram and makes swap if nessary
-	ram="$(awk '/MemTotal/ {print $2}' /proc/meminfo)"
-	minram="1048576"
-	if [ "$ram" -le "$minram" ]
-	then
-		dd if=/dev/zero of=/swapfile count=2048 bs=1M
-		chmod 600 /swapfile
-		mkswap /swapfile
-		swapon /swapfile
-		echo "/swapfile   none    swap    sw    0   0" > /etc/fstab
-	fi
-	# Download and Compile 
-	git clone https://github.com/zocteam/zeroonecoin
-	cd zeroonecoin
-	sudo ./autogen.sh
-	sudo ./configure CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768"
-	cpucores = grep -c ^processor /proc/cpuinfo
-	sudo make -j$cpucores
-	mkdir ~/zeroone
-	mv ~/zeroonecoin/src/zerooned ~/zeroone/zerooned
-	mv ~/zeroonecoin/src/zeroone-cli ~/zeroone/zeroone-cli
-	mv ~/zeroonecoin/src/zeroone-tx ~/zeroone/zeroone-tx
-}
-
-askforprivatekey(){
-#Masternode Priv Key
-	read privkey
-	while true; do
-		read -p "Is This The Correct Private Key : ${privkey} ? [Y/N]" yn
-		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) echo "Please Type In The Correct Private Key"; read privkey;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-}
-
-askstartonboot(){
-echo "**************************************************************"
-	while true; do
-		read -p "Would you like to start the node automatically on boot?  [Y/N]" yn
-		case $yn in
-			[Yy]* ) startonboot;break;;
-			[Nn]* ) break;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-}
-
-startonboot(){
-	chmod -x startZeroOne.sh
-	chmod 777 startZeroOne.sh
-	sudo echo "@reboot /root/startZeroOne.sh" >> /etc/crontab
-}
-
-genkey()
-{
-		zeroone/zerooned -daemon
-		echo "Waiting for ZeroOne To Start So Can Genkey"
-		sleep 10
-		while ! zeroone/zeroone-cli getinfo; do
-			sleep 10
-		done
-		privkey=$(zeroone/zeroone-cli masternode genkey)
-		killall zerooned
-}
 info()
 {
 
 	echo "*******************************************************************************"
+	echo "                                Information"
+	echo "*******************************************************************************"
+	echo "In your local wallet, please append the following to masternode.conf:"
+	echo ""
+	echo "MN-X ${vpsip}:10000 ${privkey} collateral_output_txid collateral_output_index"
+	echo ""
 	echo "To manually start the node run            : zeroone/zerooned -daemon "
 	echo "To check the status of the masternode run : zeroone/zeroone-cli getinfo "
 	echo ""
@@ -244,7 +266,8 @@ info()
 
 }
 
-install_mn(){
+install_mn()
+{
 #Checks Versions
 release=$(lsb_release -r -s)
 case $release in
@@ -258,15 +281,6 @@ case $release in
 	compile;;
 	esac
 }
-setup_manager()
-{
-	killall zerooned
-	wget https://raw.githubusercontent.com/zocteam/zoc-tools/master/mnchecker
-	chmod 777 mnchecker
-	echo "rpcport=10101" >> .zeroonecore/zeroone.conf
-	sudo echo "*/10 * * * * /root/mnchecker --currency-bin-cli=/root/zeroone/zeroone-cli --currency-bin-daemon=/root/zeroone/zerooned --currency-datadir=.zeroonecore" >> /etc/crontab
-
-}
 
 #checks args then runs the functions
 case $1 in
@@ -274,9 +288,9 @@ compile)
 	install_preqs
 	compile
 	configQuestions
+	sentinel
 	config
 	start_mn
-	sentinel
 	info;;
 manager)
 	setup_manager;;
@@ -284,9 +298,9 @@ manager)
 	install_preqs
 	install_mn
 	configQuestions
+	sentinel
 	config
 	start_mn
-	sentinel
 	info
 ;;
 esac
