c76c30cff8decbed8f2ee1bf40c8ec33434c4daa
Ca.Men <camenlx@users.noreply.github.com>
2020-06-25 04:50:08 +0100

    runtime with 10 digits
    runtimerun with 10 digits

--- zoc_mn_queue.sh
+++ zoc_mn_queue.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-echo -e "nr\tstartorpaid\tcurrentime\tpayee-payee-payee-payee-payee-paye\truntime\tlastpaidtime\tstarttime"
+echo -e "nr\tstartorpaid\tcurrentime\tpayee-payee-payee-payee-payee-paye\truntimerun\tlastpaidtime\tstarttime"
 mn_queue(){
 ~/zeroone/zeroone-cli masternode list full \
         | grep "[^_]ENABLED 7" \
-        | awk -v date="$(date +%s)" '{as=date-$7; mst=(as>$8?as:$8); ze=sprintf("%010d",$8); sep="\t" ; print mst sep date sep $5 sep $7 sep ze sep as}' \
+        | awk -v date="$(date +%s)" '{as=date-$7; mst=(as>$8?as:$8); ru=sprintf("%010d",$7); ze=sprintf("%010d",$8); sep="\t" ; print mst sep date sep $5 sep ru sep ze sep as}' \
         | sort -s -k1,1n \
         | awk '{print NR "\t" $0}'
 }
 mn_queue
-
